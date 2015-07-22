fd02d1bcbecdc667416959b4410530e738c137db
TahirMehmood <tahir.mehmood.bahawalpur@gmail.com>
2015-07-22 15:02:19 +0500

    blog after resolve application.rb issue

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-  #for push in heroku
-  config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,5 +22,7 @@ module Blog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #for push in heroku
+    config.assets.initialize_on_precompile = false
   end
 end
